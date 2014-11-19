7394c52b9021cbd7089f44e51944f72270c54c6f
Lan Xu <snowery.xu@gmail.com>
2014-11-19 09:32:14 -0800

    insert comments to db

--- src/main/java/cmpe226/project2/books/cassandra/DataLoader.java
+++ src/main/java/cmpe226/project2/books/cassandra/DataLoader.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map.Entry;
@@ -16,11 +17,19 @@ import com.datastax.driver.core.BatchStatement;
 import com.datastax.driver.core.Row;
 import com.datastax.driver.core.Session;
 import com.datastax.driver.core.Statement;
+import com.datastax.driver.core.UDTValue;
+import com.datastax.driver.core.UserType;
 import com.datastax.driver.core.exceptions.InvalidQueryException;
 import com.datastax.driver.core.querybuilder.Insert;
 import com.datastax.driver.core.querybuilder.QueryBuilder;
 import com.datastax.driver.core.utils.UUIDs;
 
+/*
+ * Create schema
+ * Load data from file to db
+ * Download file from db
+ * 
+ */
 public class DataLoader {
 
 	private String keyspace = "cmpe226";
@@ -53,11 +62,11 @@ public class DataLoader {
 
 		// create book table with basic information
 		String createBookTable = String
-				.format("CREATE TABLE IF NOT EXISTS %s.%s (id uuid PRIMARY KEY, title text, author text, first_author text, release_date text, avg_rate double, count_rate int, comments frozen<%s>, content blob);",
+				.format("CREATE TABLE IF NOT EXISTS %s.%s (id uuid PRIMARY KEY, title text, author text, first_author text, release_date text, avg_rate double, count_rate int, comments set<frozen<%s>>, content blob);",
 						keyspace, bookTable, commentType);
 
 		String createUserTable = String
-				.format("CREATE TABLE IF NOT EXISTS %s.%s (id uuid PRIMARY KEY, name text, read_books frozen<%s>, write_books set<uuid>);",
+				.format("CREATE TABLE IF NOT EXISTS %s.%s (id uuid PRIMARY KEY, name text, read_books set<frozen<%s>>, write_books set<uuid>);",
 						keyspace, userTable, readType);
 		
 		String createUserNameIndex = String
@@ -75,7 +84,7 @@ public class DataLoader {
 		session.execute("DROP KEYSPACE " + keyspace);
 	}
 
-	public void addColumn(String table, String column, String type) {
+	private void addColumn(String table, String column, String type) {
 		try {
 			String addCol = String.format("ALTER TABLE %s.%s ADD %s %s",
 					keyspace, table, column, type);
@@ -110,7 +119,9 @@ public class DataLoader {
 		return statement;
 	}
 
-	public void insert(HashMap<String, String> data, String file_path) throws IOException {
+	public void insert(String file_path) throws IOException {
+		HashMap<String, String> data = new BookParser(file_path).processLineByLine();
+		
 		UUID book_id = UUIDs.random();
 		
 		Insert insertBook = QueryBuilder.insertInto(keyspace, bookTable)
@@ -139,6 +150,7 @@ public class DataLoader {
 	public void readBook(UUID id) throws IOException {
 		Statement readFile = QueryBuilder.select("content", "title").from(keyspace, bookTable).where(QueryBuilder.eq("id", id));
 		Row file = session.execute(readFile).one();
+
 		if (file != null) {
 			ByteBuffer fileBytes = file.getBytes( "content" );
 			Files.write(Paths.get("download.txt"), fileBytes.array());
@@ -150,30 +162,51 @@ public class DataLoader {
 		}
 	}
 
-	public void loadData() throws IOException {
-		String bookDir = System.getProperty("user.dir") + "/books/";
+	// load book data
+	public void loadData(String bookDir) throws IOException {
 		String[] books = new File(bookDir).list(); // { "1010.txt" }; 
 		int count = 0;
 
 		for (String book : books) {
-			String book_path = bookDir + book;
-			BookParser parser = new BookParser(book_path);
-			insert(parser.processLineByLine(), book_path);
+			insert(bookDir + book);
 			count++;
 		}
 		System.out.printf("%d books loaded.", count);
 	}	
 	
-	public static void main(String[] args) throws IOException {
+	// insert comments
+	public void insertComment(UUID book_id, UUID user_id, String comment) throws InterruptedException {
+		UserType commentT = session.getCluster().getMetadata().getKeyspace(keyspace).getUserType(commentType);
+		System.out.println(commentT.getFieldNames());
+		
+		UDTValue c = commentT.newValue()
+				.setUUID("user_id", user_id)
+				.setString("comment", comment)
+				.setDate("post_date", new Date());
+		
+		Statement statement = QueryBuilder.update(keyspace, bookTable)
+				.with(QueryBuilder.add("comments", c))
+				.where(QueryBuilder.eq("id", book_id));
+		
+		session.execute(statement);
+	}
+
+	public static void main(String[] args) throws IOException, InterruptedException {
 		CassandraClient client = new CassandraClient("localhost");
 		
 		try {
 			DataLoader loader = new DataLoader(client.getSession());
-			loader.dropKeyspace();
-			loader.init();
-			loader.addColumn("books", "language", "text");
 			
-			loader.loadData();
+//			// init schema
+//			loader.dropKeyspace();
+//			loader.init();
+//			
+//			// load all files under folder
+//			loader.loadData(System.getProperty("user.dir") + "/books/");
+			
+			// add comment
+			loader.insertComment(UUID.fromString("abd17db3-bc4f-4c5e-a096-3968d64a1bc7"), 
+					UUID.fromString("415394aa-2e1f-4d75-a7d1-ed5202b51e9d"), "comment test");
 			
 //			loader.readBook(UUID.fromString("372d532e-20f2-456c-ab1d-217b8ead54c6"));
 			
