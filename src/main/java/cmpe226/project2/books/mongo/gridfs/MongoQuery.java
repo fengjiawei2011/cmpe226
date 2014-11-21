5e40adcc69bdf7458b316f4356886622b83553b0
lingzhang-lyon <zhangling.ice@gmail.com>
2014-11-20 21:33:24 -0800

    update mongoQuery and delete files not in use
    finish insert user, update read/write books, update comments for book

--- src/main/java/cmpe226/project2/books/mongo/gridfs/MongoQuery.java
+++ src/main/java/cmpe226/project2/books/mongo/gridfs/MongoQuery.java
@@ -2,6 +2,7 @@ package cmpe226.project2.books.mongo.gridfs;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 
 import com.mongodb.BasicDBObject;
@@ -76,18 +77,65 @@ public class MongoQuery {
 	
 	
 	public static void insertUser(DB db, String username){		
-		
+		DBCollection users=db.getCollection("users");
+		BasicDBObject o = new BasicDBObject();
+		o.append("username", username);
+		users.insert(o);
 	}
 	
 	public static void addWriteBookForUser(DB db, String username, String bookid){
 		
+		
+		DBCollection col = db.getCollection("users");
+
+		BasicDBObject query = new BasicDBObject();
+		query.put("username", username); //search for the user
+
+		BasicDBObject newDocument = new BasicDBObject();
+		newDocument.put("WriteBook", bookid);
+
+		BasicDBObject updateObj = new BasicDBObject();
+		updateObj.put("$push", newDocument);
+
+		col.update(query, updateObj);
+		
+		
+		
 	}
 	
 	public static void addReadBookForUser(DB db, String username, String bookid, int rate){
+		DBCollection col = db.getCollection("users");
+
+		BasicDBObject query = new BasicDBObject();
+		query.put("username", username); //search for the user
+
+		BasicDBObject newDocument = new BasicDBObject();
+		newDocument.put("ReadBook", new BasicDBObject("Book", bookid)
+											.append("Rating", rate) );
+
+		BasicDBObject updateObj = new BasicDBObject();
+		updateObj.put("$push", newDocument);
+
+		col.update(query, updateObj);
+		
 		
 	}
 	
 	public static void insertComment(DB db, String bookid, String userid, String comment){
-		
+		DBCollection col = db.getCollection("books_meta");
+
+		BasicDBObject query = new BasicDBObject();
+		query.put("md5", bookid); //search for the book
+
+		BasicDBObject newDocument = new BasicDBObject();
+		newDocument.put("comments", new BasicDBObject("User", userid)
+											.append("Comment", comment)
+											.append("PostDate", new Date(System.currentTimeMillis()) )
+											);
+
+		BasicDBObject updateObj = new BasicDBObject();
+		updateObj.put("$push", newDocument);
+
+		col.update(query, updateObj);
 	}
 }
