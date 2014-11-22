3700e2a1063dffa29d35d96f3d7c706a0ecc16c1
lillianlin <lillianlin@users.noreply.github.com>
2014-11-21 16:44:59 -0800

    Update QueryTest.java
    add 3 Cassandra queries for QueryTest.java

--- src/main/java/cmpe226/project2/books/cassandra/QueryTest.java
+++ src/main/java/cmpe226/project2/books/cassandra/QueryTest.java
@@ -13,7 +13,7 @@ public class QueryTest {
 	
 	// Load books
 	public void loadBooks () throws IOException {
-		dao.recreateKeyspace();
+		dao.recreateKeyspace();		
 		
 		long begin = System.currentTimeMillis();
 		
@@ -24,16 +24,52 @@ public class QueryTest {
 		System.out.println("Total used " + (end - begin) + " msec");
 	}
 	
-	//TODO
-	public void query() {
+	// Query 1
+	public void querySearchByTitle() {
 		
+		long begin = System.currentTimeMillis();
+		
+		dao.searchByTitle();
+		
+		long end = System.currentTimeMillis();
+		
+		System.out.println("Total used " + (end - begin) + " msec");
 	}
+	
+	// Query 2
+	public void querySearchByAuthor() {
 
+		long begin = System.currentTimeMillis();
+		
+		dao.searchByAuthor();
+		
+		long end = System.currentTimeMillis();
+		
+		System.out.println("Total used " + (end - begin) + " msec");
+	}
+	
+	// Query 3
+	public void querySearchByLanguage() {
+
+		dao.addIndex("books", "language");
+
+		long begin = System.currentTimeMillis();
+		
+		dao.searchByLanguage();
+		
+		long end = System.currentTimeMillis();
+		
+		System.out.println("Total used " + (end - begin) + " msec");
+	}
+	
 	
 	public static void main(String[] args) throws IOException {
 		QueryTest test = new QueryTest();
 		
 		test.loadBooks();
+		test.querySearchByTitle();
+		test.querySearchByAuthor();
+		test.querySearchByLanguage();
 	}
 	
 }
